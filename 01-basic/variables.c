426d7dbc446c2f95cba850599763a52e1f734a8d
YuhaoGao <3244318053@qq.com>
2026-04-08 23:07:49 +0800

    feat :add variables practice

---
+++ 01-basic/variables.c
@@ -0,0 +1,15 @@
+// 01-basic/variables.c
+#include <stdio.h>
+
+int main() {
+    // 定义不同类型变量
+    int age = 20;
+    float score = 95.5;
+    char grade = 'A';
+
+    printf("年龄：%d\n", age);
+    printf("分数：%.1f\n", score);
+    printf("等级：%c\n", grade);
+
+    return 0;
+}
